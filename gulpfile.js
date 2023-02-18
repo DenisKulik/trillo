66cef15b554580ec4a7da8250e9c5a811b1e036d
Denis Kulik <kulikdenis1996x@gmail.com>
2023-02-18 15:57:48 +0700

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+// Main module
+import gulp from 'gulp';
+// paths
+import { path } from './gulp/config/path.js';
+// general plugins
+import { plugins } from './gulp/config/plugins.js';
+
+global.app = {
+  isBuild: process.argv.includes('--build'),
+  isDev: !process.argv.includes('--build'),
+  path: path,
+  gulp: gulp,
+  plugins: plugins,
+};
+
+// import tasks
+import { copy } from './gulp/tasks/copy.js';
+import { reset } from './gulp/tasks/reset.js';
+import { html } from './gulp/tasks/html.js';
+import { server } from './gulp/tasks/server.js';
+import { scss } from './gulp/tasks/scss.js';
+import { js } from './gulp/tasks/js.js';
+import { images } from './gulp/tasks/images.js';
+import { otfToTtf, ttfToWoff, fontsStyle } from './gulp/tasks/fonts.js';
+import { svgSprive } from './gulp/tasks/svgSprive.js';
+import { zip } from './gulp/tasks/zip.js';
+
+// observer for changes in files
+function watcher() {
+  gulp.watch(path.watch.files, copy);
+  gulp.watch(path.watch.html, html);
+  gulp.watch(path.watch.scss, scss);
+  gulp.watch(path.watch.js, js);
+  gulp.watch(path.watch.images, images);
+}
+
+export { svgSprive };
+
+// Conservative processing of fonts
+const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
+
+// Main tasks
+const mainTasks = gulp.series(
+  fonts,
+  gulp.parallel(copy, html, scss, js, images)
+);
+
+// Building script for completing tasks
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
+const build = gulp.series(reset, mainTasks);
+const deployZip = gulp.series(reset, mainTasks, zip);
+
+// export
+export { dev };
+export { build };
+export { deployZip };
+
+// default script
+gulp.task('default', dev);
